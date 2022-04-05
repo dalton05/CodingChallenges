612710560ca01a5ad3ffaab454935f51105d955b
dalton05 <dalton05@gmail.com>
2022-04-04 19:36:37 -0600

    final commit of this challenge

--- Remove-anchor-from-URL/app.js
+++ Remove-anchor-from-URL/app.js
@@ -3,10 +3,10 @@
 // For example: "www.codewars.com#about" --> "www.codewars.com"
 //              "www.codewars.com?page=1" -->"www.codewars.com?page=1"
 
-// Qu
-
+// Parameters: (thoughts, edge cases, etc.)
+// Examples:
+// Pseudo Code:
 
 function removeUrlAnchor(url){
     return url.split('#')[0]
   }
-
